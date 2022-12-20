3049ab49ac71ff6bb12c713dae3bd96279437910
C Martin <cmart131@yahoo.com>
2022-12-19 19:48:34 -0800

    added embedded map into HTML

--- script.js
+++ script.js
@@ -5,27 +5,43 @@ const search = document.getElementById("search-button");
 
 
 
+var mapKey = "T0AABSUg4vasWZxGxVRqmARpHR0d3wJc"
 
 
+function map(input) {
+     input = $('#city_input').val().trim();
+     var queryURL="https://www.mapquestapi.com/search/v4/place?sort=distance&feedback=false&key=T0AABSUg4vasWZxGxVRqmARpHR0d3wJc";
+   
+     fetch(queryURL)
+       .then(function (response) {
+         return response.json();
+       })
+       .then(function (data) {
+         console.log(data);
+        
+          $("#map").append(input);
+   });
 
-
-
+}
+search.addEventListener("click", map);
 //Yelp Fusion API
 
-API Key =
-XrJ9PFPKIcRiTDcpixQl8_Kw3v00qbkA0sq9CUoJWDymks3sdpATOA9X7Kk2vrx-rlpPg9iEsiwQmaiBTb-HwAyHV8gfTOTzyu_0DHSuqnmVhRCC83MM_ZZ82vebY3Yx
+// API Key =
+// XrJ9PFPKIcRiTDcpixQl8_Kw3v00qbkA0sq9CUoJWDymks3sdpATOA9X7Kk2vrx-rlpPg9iEsiwQmaiBTb-HwAyHV8gfTOTzyu_0DHSuqnmVhRCC83MM_ZZ82vebY3Yx
 
-curl --request GET \
-     --url https://api.yelp.com/v3/events/awesome-event \
-     --header 'Authorization: Bearer API_KEY' \
-     --header 'accept: application/json'
+// curl --request GET \
+//      --url https://api.yelp.com/v3/events/awesome-event \
+//      --header 'Authorization: Bearer API_KEY' \
+//      --header 'accept: application/json'
 
 
 
-//mapQuest API key
 
-     API Key = T0AABSUg4vasWZxGxVRqmARpHR0d3wJc
 
 //Bing maps
 
-API key =  Al7OdiPyzEykkQtq9N0tfF2-85LosZCbAMvSxWxZ0nREKBqTififdRTwb_Zai0pK
+
+
+//Al7OdiPyzEykkQtq9N0tfF2-85LosZCbAMvSxWxZ0nREKBqTififdRTwb_Zai0pK
+
+https://spatial.virtualearth.net/REST/v1/data/a1b18b9f4d024416afd5643c2e1f6d4e/Bing/FourthCoffeeShops	
